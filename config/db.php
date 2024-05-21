9b18dedfffe478b7321b2dfd47a69a5097706aa3
EduardoTnc <129677089+EduardoTnc@users.noreply.github.com>
2024-05-21 13:18:33 -0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -13,5 +13,5 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Error connecting to the database: " . $e->getMessage());
+    die("Error al conectarse a la Base de Datos: " . $e->getMessage());
 }
